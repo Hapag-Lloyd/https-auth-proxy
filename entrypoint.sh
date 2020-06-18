3e551f3cf18df5986ebf6c4f6c1508bcdf6fdffe
Jaemin Nam <jaemin.nam@hlag.com>
2020-06-18 16:08:26 +0200

    https-auth-proxy -raf/en/jn- password magic test 2
    Signed-off-by: Jaemin Nam <jaemin.nam@hlag.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ openssl req -x509 -nodes -days 3650 -subj "/C=DE/ST=Hamburg/O=https-auth-proxy/C
             -keyout /tmp/nginx/nginx-selfsigned.key -out /tmp/nginx/nginx-selfsigned.crt
 
 # generate encrypted password by using secret variable
-echo ${ADMIN_PASSWD}
+echo "${ADMIN_PASSWD}"
 htpasswd -c -b /etc/nginx/.htpasswd admin "${ADMIN_PASSWD}"
 
 #test
