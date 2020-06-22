e1d65927a4f1806148fc388767953cb5508c0aa5
fluegra <rasmus.fluegge@hlag.com>
2020-06-22 14:39:59 +0200

    https-auth-proxy -td/en/jn/raf- ITADC-91 merged files with newer version

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,7 @@ openssl req -x509 -nodes -days 3650 -subj "/C=DE/ST=Hamburg/O=https-auth-proxy/C
             -keyout /tmp/nginx/nginx-selfsigned.key -out /tmp/nginx/nginx-selfsigned.crt
 
 # generate encrypted password by using secret variable
-echo "${ADMIN_PASSWD}"
 htpasswd -c -b /etc/nginx/.htpasswd admin "${ADMIN_PASSWD}"
-
-#test
-cat /etc/nginx/.htpasswd
 
 # run additional commands given by Dockerfile
 exec "$@"
