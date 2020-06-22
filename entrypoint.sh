ed60b21bea919b730b9fffe8195a95571007f195
fluegra <rasmus.fluegge@hlag.com>
2020-06-22 14:37:44 +0200

    https-auth-proxy -td/en/jn/raf- ITADC-91 updated healthcheck and yeah... don't know. stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-sed -i "s/INPORT/${INPORT}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/TARGET_HOST_PORT/${TARGET_HOST_PORT}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/HEALTHCHECK_PATH/${HEALTHCHECK_PATH}/g" /etc/nginx/conf.d/default.conf
 
 openssl req -x509 -nodes -days 3650 -subj "/C=DE/ST=Hamburg/O=https-auth-proxy/CN=localhost" \
             -addext "subjectAltName=DNS:localhost" -newkey rsa:2048 \
