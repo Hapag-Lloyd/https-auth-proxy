7d53bd06a3d3a19a5ad3dd5caad96415ab816fff
fluegra <rasmus.fluegge@hlag.com>
2020-07-30 14:25:36 +0200

    https-auth-proxy -jn/raf- QA: updated informational lines in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 set -x
 
+# add internal target information to the nginx config
 sed -i "s,TARGET_HOST_PORT,${TARGET_HOST_PORT},g" /etc/nginx/conf.d/default.conf
 sed -i "s,HEALTHCHECK_PATH,${HEALTHCHECK_PATH},g" /etc/nginx/conf.d/default.conf
 
+# generate ssl (https) certificate and key
 openssl req -x509 -nodes -days 3650 -subj "/C=DE/ST=Hamburg/O=https-auth-proxy/CN=localhost" \
             -addext "subjectAltName=DNS:localhost" -newkey rsa:2048 \
             -keyout /tmp/nginx/nginx-selfsigned.key -out /tmp/nginx/nginx-selfsigned.crt
