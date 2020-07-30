1c3869bbca5e749ffdf6aabc80d4c5323f444ae5
fluegra <rasmus.fluegge@hlag.com>
2020-07-30 14:21:21 +0200

    https-auth-proxy -jn/raf- QA: added informational line about credentials to the docker push script

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
+# Information about user and password can be found in the README.md file
 docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
 docker push hlag/https-auth-proxy:latest
